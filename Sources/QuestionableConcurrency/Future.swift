64fe40422e58494108700440661c0da6bde403e1
Dimitri Bouniol <dimitribouniol@mochidev.com>
2026-04-19 07:01:09 -0700

    Added an `AsyncResult` and `Future` type

---
+++ Sources/QuestionableConcurrency/Future.swift
@@ -0,0 +1,11 @@
+//
+//  Future.swift
+//  https://github.com/mochidev/swift-questionable-concurrency
+//
+//  Created by Dimitri Bouniol on 2026-04-16.
+//  Copyright © 2026 Mochi Development, Inc. All rights reserved.
+//  swift-questionable-concurrency-watermark: 20E931FAE8CA4B05929CA61A82D9DA19
+//
+
+/// An asynchronous value or result that can only be fullfilled by an associated ``Promise``.
+public typealias Future = AsyncResult
