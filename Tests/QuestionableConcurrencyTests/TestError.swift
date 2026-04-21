036ff37b1b112c6f336b3e949a076ebabab748a0
Dimitri Bouniol <dimitribouniol@mochidev.com>
2026-04-21 04:40:35 -0700

    Added tests for `AsyncResult`

---
+++ Tests/QuestionableConcurrencyTests/TestError.swift
@@ -0,0 +1,10 @@
+//
+//  TestError.swift
+//  https://github.com/mochidev/swift-questionable-concurrency
+//
+//  Created by Dimitri Bouniol on 2026-04-21.
+//  Copyright © 2026 Mochi Development, Inc. All rights reserved.
+//  swift-questionable-concurrency-watermark: 20E931FAE8CA4B05929CA61A82D9DA19
+//
+
+struct TestError: Error, Hashable {}
